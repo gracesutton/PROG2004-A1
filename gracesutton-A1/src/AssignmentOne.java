f272ec98f48641555867a0cc4d5b3bc9dd45e0ec
gracesutton <gracesutton20@gmail.com>
2024-11-04 23:27:12 +1000

    Updated Appointment Class
    Created instance variables
    Default constructor
    A second constructor that initialises all the instance variables.
    Getter and setter methods with validation.
    printApptInfo() method.
    
    Other small modifications to project.
    Created 4 new objects.
    Added setter methods for HealthProfessional class.

--- gracesutton-A1/src/AssignmentOne.java
+++ gracesutton-A1/src/AssignmentOne.java
@@ -1,13 +1,23 @@
 public class AssignmentOne {
     public static void main(String[] args) throws Exception {
+
+        // Part 3 – Using classes and objects
         
         GeneralPractitioner gp1 = new GeneralPractitioner(1001, "Dr. Grace Sutton", "F");
-        OccupationalTherapist ot1 = new OccupationalTherapist(1002, "Paige Robinson", "F");
+        GeneralPractitioner gp2 = new GeneralPractitioner(1002, "Dr. Crawford James", "M");
+        GeneralPractitioner gp3 = new GeneralPractitioner(1003, "Dr. Patrick Ward", "M");
+
+        OccupationalTherapist ot1 = new OccupationalTherapist(100, "Paige Robinson", "A");
+        OccupationalTherapist ot2 = new OccupationalTherapist(1005, "Callum Daniel", "M");
         
         gp1.printInfo(); 
-        ot1.printInfo();
+        gp2.printInfo();
+        gp3.printInfo();
 
-        // HealthProfessional gp2 = new HealthProfessional("1", "Dr. Grace Sutton", "F");
+        ot1.printInfo();
+        ot2.printInfo();
+
+        System.out.println("\n------------------------------\n");
 
     }
 }
@@ -52,18 +62,36 @@ class HealthProfessional {
         if (ID >= 1000) {
             this.ID = ID;
         } else {
-            System.out.println("Invalid ID: ID must be 4 or more digits.");
+            System.out.println("Invalid ID: Must be 4 or more digits.");
+        }
+    }
+
+    public void setName(String name) {
+        //perform input validation
+        if ((name.length() > 3) && (name.length() < 30)) {
+            this.name = name;
+        } else {
+            System.out.println("Invalid Name: Must be between 3 and 30 characters.");
+        }
+    }
+
+    public void setGender(String gender) {
+        this.gender.toUpperCase();
+        if ((gender.equals("M")) || (gender.equals("F"))) {
+            this.gender = gender;
+        } else {
+            System.out.println("Invalid Gender: Must be either M or F.");
         }
     }
 
     // public void setName
 
     public void printInfo() {
-        System.out.println("The health professional details are:");
+        System.out.println("\nThe health professional details are:\n");
         System.out.println("ID: " + ID);
         System.out.println("Name: " + name);
         System.out.println("Gender: " + gender);
-        
+
     }
 }
 
@@ -77,7 +105,7 @@ class GeneralPractitioner extends HealthProfessional {
     }
 
     public String getType() {
-        return getType();
+        return type;
     }
 
     public void printInfo() {
@@ -97,7 +125,7 @@ class OccupationalTherapist extends HealthProfessional {
     }
 
     public String getType() {
-        return getType();
+        return type;
     }
 
     public void printInfo() {
@@ -108,5 +136,81 @@ class OccupationalTherapist extends HealthProfessional {
 }
 
 class Appointment {
+
+    private String ptName;
+    private String ptPhone;
+    private String timeSlot;
+    private HealthProfessional selectedDr;
+
+    // Default Constructor
+    public Appointment() {
+        this.ptName = null;
+        this.ptPhone = null;
+        this.timeSlot = null;
+        this.selectedDr = null;
+    }
+    
+    // Parameterised Constructor
+    public Appointment(String ptName, String ptPhone, String timeSlot, HealthProfessional selectedDr) {
+        this.ptName = ptName;
+        this.ptPhone = ptPhone;
+        this.timeSlot = timeSlot;
+        this.selectedDr = selectedDr;
+    }
+
+    public String getPtName() {
+        return ptName;
+    }
+
+    public String getPtPhone() {
+        return ptPhone;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public String getSelectedDr() {
+        return selectedDr.getName();
+    }
+
+    public void setPtName(String ptName) {
+        //perform input validation
+        if ((ptName.length() > 3) && (ptName.length() < 30)) {
+            this.ptName = ptName;
+        } else {
+            System.out.println("Invalid Name: Must be between 3 and 30 characters.");
+        }
+    }
+
+    public void setPtPhone(String ptPhone) {
+        if (ptPhone.length() == 10) {
+            this.ptPhone = ptPhone;
+        } else {
+            System.out.println("Invalid Phone Number. Please try again.");
+        }
+    }
+
+    public void setTimeSlot(String timeSlot) {
+        //perform input validation
+        if ((timeSlot.length() == 4) && (timeSlot.length() < 30)) {
+            this.timeSlot = timeSlot;
+        } else {
+            System.out.println("Invalid Timeslot. Please enter a valid time in the format 00:00.");
+        }
+    }
+
+
+    // Method to print all information
+    public void printApptInfo() {
+        System.out.println("\nThe appointment details are:\n");
+        System.out.println("Patient Name: " + ptName);
+        System.out.println("Patient Phone Number: " + ptPhone);
+        System.out.println("Time: " + timeSlot);
+        System.out.println("Doctor: " + selectedDr.getName());
+    }
+    
+
+
 
 }
