10190417a9e3cc62a7c8c831d322bcbe0b96b927
gracesutton <gracesutton20@gmail.com>
2024-11-10 23:53:44 +1000

    Updated main class for pt.5
    createAppointment() method
    printExistingAppointments() method
    cancelBooking() method
    Imported LocalTime & Iterator package.

--- gracesutton-A1/src/AssignmentOne.java
+++ gracesutton-A1/src/AssignmentOne.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.time.LocalTime;
+import java.util.Iterator;
+
 public class AssignmentOne {
     public static void main(String[] args) throws Exception {
 
         // Part 3 – Using classes and objects
         
+        // create some instance objects for GP and OT classes
         GeneralPractitioner gp1 = new GeneralPractitioner(1001, "Dr. Grace Sutton", "F");
         GeneralPractitioner gp2 = new GeneralPractitioner(1002, "Dr. Crawford James", "M");
         GeneralPractitioner gp3 = new GeneralPractitioner(1003, "Dr. Patrick Ward", "M");
@@ -10,6 +15,7 @@ public class AssignmentOne {
         OccupationalTherapist ot1 = new OccupationalTherapist(100, "Paige Robinson", "A");
         OccupationalTherapist ot2 = new OccupationalTherapist(1005, "Callum Daniel", "M");
         
+        // print details of each object
         gp1.printInfo(); 
         gp2.printInfo();
         gp3.printInfo();
@@ -19,7 +25,72 @@ public class AssignmentOne {
 
         System.out.println("\n------------------------------\n");
 
+        // Part 5 – Collection of appointments
+
+        // create some appointments
+        createAppointment("Elsie McLaughlin", "0413 999 838", LocalTime.of(8, 0), gp1);
+        createAppointment("John Paddy", "0422 989 727", LocalTime.of(14,30), gp3);
+        createAppointment("Jade Clarke", "0413 369 248", LocalTime.of(12, 15), ot1);
+        createAppointment("Ben Michaels", "0408 764 818", LocalTime.of(11, 45), ot2);
+
+        // print existing appointments
+        printExistingAppointments();
+
+        // cancel an appointment
+        cancelBooking("0408 764 816");
+        cancelBooking("0408 764 818");
+
+        // print again
+        printExistingAppointments();
+
+        System.out.println("------------------------------");
     }
+
+    private static ArrayList<Appointment> appointments = new ArrayList<>(); // new ArrayList to store all appointments (Appointment objects)
+
+    // method to create a new booking and add it to the ArrayList
+    public static void createAppointment(String ptName, String ptPhone, LocalTime timeSlot, HealthProfessional selectedDr) {
+        Appointment apt1 = new Appointment(ptName, ptPhone, timeSlot, selectedDr);
+        appointments.add(apt1);
+        System.out.println("Appointment created for " + ptName + " (ph: " + ptPhone + ") at " + timeSlot + " with " + selectedDr.getName());
+    }
+
+    // method to display existing appointments in the ArrayList
+    public static void printExistingAppointments() {
+        if (appointments.isEmpty()) {
+            System.out.println("There are no existing appointments.");
+        } else {
+            System.out.println("\n** Existing Appointments **\n");
+            for (Appointment appointment : appointments) {
+                appointment.printApptInfo();
+            }
+        }
+    }
+
+    // method to cancel a booking using a patient's phone number
+    public static void cancelBooking(String ptPhone) {
+
+        System.out.println("** Request to remove booking for " + ptPhone + " **\n");
+        boolean found = false;
+
+        Iterator<Appointment> iter = appointments.iterator();
+        while (iter.hasNext()) {
+
+            Appointment appointment = iter.next();
+
+            if (appointment.getPtPhone().equals(ptPhone)) {
+                found = true;
+                appointments.remove(appointment);
+                System.out.println("Appointment for '" + appointment.getPtName() + " ph " + ptPhone + "' has been removed.");
+                break;
+            } 
+        }
+        
+        if (found == false) {
+            System.out.println("Appointment not found.\n");
+        }
+    }
+        
 }
 
 // Base class
@@ -139,7 +210,7 @@ class Appointment {
 
     private String ptName;
     private String ptPhone;
-    private String timeSlot;
+    private LocalTime timeSlot;
     private HealthProfessional selectedDr;
 
     // Default Constructor
@@ -151,7 +222,7 @@ class Appointment {
     }
     
     // Parameterised Constructor
-    public Appointment(String ptName, String ptPhone, String timeSlot, HealthProfessional selectedDr) {
+    public Appointment(String ptName, String ptPhone, LocalTime timeSlot, HealthProfessional selectedDr) {
         this.ptName = ptName;
         this.ptPhone = ptPhone;
         this.timeSlot = timeSlot;
@@ -166,7 +237,7 @@ class Appointment {
         return ptPhone;
     }
 
-    public String getTimeSlot() {
+    public LocalTime getTimeSlot() {
         return timeSlot;
     }
 
@@ -203,11 +274,10 @@ class Appointment {
 
     // Method to print all information
     public void printApptInfo() {
-        System.out.println("\nThe appointment details are:\n");
         System.out.println("Patient Name: " + ptName);
         System.out.println("Patient Phone Number: " + ptPhone);
         System.out.println("Time: " + timeSlot);
-        System.out.println("Doctor: " + selectedDr.getName());
+        System.out.println("Doctor: " + selectedDr.getName() + "\n");
     }
     
 
